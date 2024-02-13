35394c9fcd502c97102e2d3df933840ece2009a7
Raulpal <rpallares@miners.utep.edu>
2024-02-13 15:56:10 -0700

    done

--- src/mytar.py
+++ src/mytar.py
@@ -5,9 +5,7 @@ from sys import argv
 from io import BufferedReader, FileIO
 
 # Creates tar file similar to command tar c file1 file2... > combined.tar
-def create(files,tar):
-
-    fdWriter = os.open(tar, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, stat.S_IRWXU)  # Get tar file to write
+def create(files):
     
     for file in files:
         if not os.path.exists(file):                         # Check if path does not exist
@@ -22,12 +20,10 @@ def create(files,tar):
 
         contents = os.read(fdReader, fileByteSize)          # Get byte data
 
-        os.write(fdWriter, fileHeader)                      # Store header file contents in tar file
-        os.write(fdWriter, contents)                        # Store data contents in tar file
+        os.write(1, fileHeader)                             # Store header file contents in tar file
+        os.write(1, contents)                               # Store data contents in tar file
 
         os.close(fdReader)                                  # Close when done
-    os.close(fdWriter)                                      # Close when done
-
 
 # Extract from .tar  file into separate files
 def extract(tar):
@@ -61,22 +57,14 @@ def extract(tar):
     tar_file.close()                                         # Close when done
 
 
-
-
 tarCommand = argv[1:]                                       # Takes TAR command input 
-
-# if(len(tarCommand) != 3):            # Check if input is not right length for create or extract.
-#     os.write(2, ("Commands are not valid\n".encode()))
     
-
 command = tarCommand[0].lower()                             # c or x
 
 if(command == 'c'):                                         # Check if command valid "c"
     os.write(1,"Creating...\n".encode())
-    # arFile = (file1.split(".".encode())[0] + file2.split(".".encode())[0] + ".tar".encode()) # Tar file
-    os.write(1, b"tar c files... > foogoo.tar\n" )
 
-    create(tarCommand[1:3], "foogoo.tar") # create and store in .tar
+    create(tarCommand[1:3])                                 # create and store in .tar
 
 if(tarCommand[0].lower() == 'x'):                           # Check if command valid "x"
     os.write(1,"Extracting...\n".encode())                  
